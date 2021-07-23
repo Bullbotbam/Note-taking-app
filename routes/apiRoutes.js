f733f5b6bcc125b2f101116e84ca447fe80ceaeb
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-23 18:51:43 -0500

    psuedo code sections for project structure

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
+const animalRoutes = require('../routes/apiRoutes');
 
-router.use(animalRoutes);
+router.use(apiRoutes);
 
-module.exports = router;
-
+// build our get for api/notes respnd with notes in database
 router.get('/notes', (req, res) => {
     let results = notes;
     if (req.query) {
@@ -12,3 +11,55 @@ router.get('/notes', (req, res) => {
     }
     res.json(results);
   });
+
+// router.post to add notes to database
+// Hey this is not complete to this code
+router.post('/vote', ({ body }, res) => {
+  // Data validation
+  const errors = inputCheck(body, 'voter_id', 'candidate_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+  const params = [body.voter_id, body.candidate_id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+      changes: result.affectedRows
+    });
+  });
+});
+
+// route to delete notes using router.delete
+//  this example will need to be corrected to meet this application
+// Delete a voter
+router.delete('/voter/:id', (req, res) => {
+  const sql = `DELETE FROM voters WHERE id = ?`;
+
+  db.query(sql, req.params.id, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Voter not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+
+module.exports = router;

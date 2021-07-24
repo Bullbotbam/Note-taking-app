2638ef4ab3651a0bb898365bfbc4081bcc50ff99
Donald Bull <dbullaustintx@gmail.com>
2021-07-24 00:25:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const { uuid } = require('uuidv4')
-const router = require('express').Router
+const express = require("express");
+const { uuid } = require("uuidv4");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const fs = require("fs");
 
+// don't forget to
+app.use("/api", apiRoutes);
 
-
-
-
-// don't forget to 
-app.use('/api', apiRoutes) 
+app.listening(PORT, () => {
+  console.log(`API server now on ${PORT}!`);
+});

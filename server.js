2c13621e6aefd367dad11127f7111d66e0500e03
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-24 11:59:30 -0500

    adjust get route

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require("fs");
 
-const { notes } = require('./db/db.json')
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const { notes } = require('./db/db.json');
+const htmlRoutes = require('./routes/htmlRoutes/html');
+const apiRoutes = require("./routes/apiRoutes/noteRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,32 +20,7 @@ app.use("/api", apiRoutes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes", (res, req) => {
-  return notes.html;
-});
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
-  });
-// app.get("/", (res, req) => {
-//   return index.html;
-// });
 
-//create post route to allow for notes to be composed
-app.post("/api/notes", (req, res) => {
-  // req.body is where our incoming content will be
-  // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateNote(req.body)) {
-    res.status(400).send("The note is not properly formatted");
-  } else {
-    // add notes to json file and notes array in this function
-    const note = createNewNote(req.body, notes);
-    res.json(req.note);
-  }
-});
 
 app.listening(PORT, () => {
   console.log(`API server now on ${PORT}!`);

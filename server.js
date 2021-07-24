385fac9905f4428dfbd0b38731b194cba752e9e3
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-07-24 04:48:08 -0500

    Error: Cannot find module 'express' Require stack:rerun this

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require("fs");
 
+const { notes } = require('./db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
